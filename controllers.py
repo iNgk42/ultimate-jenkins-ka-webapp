79b32560f1376d6e5ecfc53d70b9a5573c17a9e0
NGUEKAM YOSSA Gabin <bobiyossa@gmail.com>
2024-03-24 02:39:15 +0100

    upgraded form args hadling and added flashed messages in register_new_task function

--- controllers.py
+++ controllers.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 from models import Task
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC'
 
 @app.route("/")
 def home():
@@ -11,18 +12,20 @@ def home():
 def register_new_task():
     if request.method == "POST":
         #get form data
-        task_id = request.form["task_id"]
         task_title = request.form["task_title"]
         task_description = request.form["task_description"]
         task_due_date = request.form["task_due_date"]
-        task_registration_date = request.form["task_registration_date"]
+
+        print("format de la date", task_due_date)
 
         #create new task object
-        task = Task(task_id, task_title, task_description, task_due_date, task_registration_date)
+        task = Task('taskID', task_title, task_description, task_due_date, 'taskRegistrationDate')
 
         #save data to database
         task.register()
         
         #redirect to confirmation page
+        flash("Your task have been successfully registered !", "message")
+        return redirect(url_for("home"))
     else:
         return render_template("register-task.html")
